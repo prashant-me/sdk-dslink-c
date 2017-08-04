b48e8d2a2ecd9a830a0c9a60236caa58b83af0b5
Michael Wielpuetz <mwielpue@cisco.com>
2017-08-04 12:12:01 +0200

    Added UUID to link names

--- tools/utils/loadtest.sh
+++ tools/utils/loadtest.sh
@@ -4,23 +4,28 @@ count="$1"
 
 #set -x
 
-broker_address=http://eff-t-cnt7-02:8080/conn
+broker_address=http://10.228.219.253:8100/conn
+#broker_address=http://eff-t-cnt7-02:8080/conn
 #broker_address=http://localhost:8080/conn
+
+UUID=$(cat /proc/sys/kernel/random/uuid)
+requester_base_name=requester-${UUID}-
+responder_base_name=responde-${UUID}-
 
 mkdir testrun
 for i in {1..3}
 do
-    
+
     mkdir testrun/load_test_pair_$i
     cp load_test_responder testrun/load_test_pair_$i
     cp load_test_requester testrun/load_test_pair_$i
 
     echo "Creating subscription path /downstream/responder${i}/rng"
-    echo -n "/downstream/responder${i}/rng" > requester$i.requestpath
+    echo -n "/downstream/${responder_base_name}${i}/rng" > ${requester_base_name}$i.requestpath
 
-    ./testrun/load_test_pair_$i/load_test_requester --broker=${broker_address} --name=requester$i &> requester_$i.out &
+    ./testrun/load_test_pair_$i/load_test_requester --broker=${broker_address} --name=${requester_base_name}$i &> requester_$i.out &
     sleep 1
-    ./testrun/load_test_pair_$i/load_test_responder --broker=${broker_address} --name=responder$i &> responder_$i.out &
+    ./testrun/load_test_pair_$i/load_test_responder --broker=${broker_address} --name=${responder_base_name}$i &> responder_$i.out &
 done
 
 read -p "Press key to stop the test... " -n1 -s
@@ -29,4 +34,4 @@ rm requester*.requestpath
 rm -rf testrun
 
 killall -9 load_test_responder
-killall -9 load_test_requester
+
